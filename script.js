953d1682cacf1278f551f0044574ce3ebf9e8614
gileludin <gileludin@gmail.com>
2025-08-04 02:44:06 +0700

    Implement playGame function

--- script.js
+++ script.js
@@ -33,30 +33,40 @@ function playRound(humanChoice, computerChoice) {
     console.log("Tie! Rock vs. Rock");
   } else if (humanChoice === "Rock" && computerChoice === "Paper") {
     console.log("You Lose! Paper beats Rock");
-    return computerScore += 1;
+    return computerScore++;
   } else if (humanChoice === "Rock" && computerChoice === "Scissor") {
     console.log("You Win! Rock beats Scissor");
-    return humanScore += 1;
+    return humanScore++;
   } else if (humanChoice === "Paper" && computerChoice === "Rock") {
     console.log("You Win! Paper beats Rock");
-    return humanScore += 1;
+    return humanScore++;
   } else if (humanChoice === "Paper" && computerChoice === "Paper") {
     console.log("Tie! Paper vs. Paper");
   } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
     console.log("You Lose! Scissor beats Paper");
-    return computerScore += 1;
+    return computerScore++;
   } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
     console.log("You Lose! Rock beats Scissor");
-    return computerScore += 1;
+    return computerScore++;
   } else if (humanChoice === "Scissor" && computerChoice === "Paper") {
     console.log("You Win! Scissor beats Paper");
-    return humanScore += 1;
+    return humanScore++;
   } else if (humanChoice === "Scissor" && computerChoice === "Scissor") {
     console.log("Tie! Scissor vs. Scissor");
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame(n) {
+  for (let i = 1; i <= n; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  console.log(`Human Score: ${humanScore}`);
+  console.log(`Human Score: ${computerScore}`);
+  if (humanScore > computerScore) {
+    console.log("You Win!");
+  } else {
+    console.log("Computer Win!");
+  }
+}
 
-playRound(humanSelection, computerSelection);
+playGame(5);
