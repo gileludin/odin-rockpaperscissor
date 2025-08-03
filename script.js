13163ff1bf6c3d65294b76008766bf81350cd559
gileludin <gileludin@gmail.com>
2025-08-04 01:55:36 +0700

    Implement getHumanChoice Function

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ function getComputerChoice() {
   }
 }
 
+function getHumanChoice() {
+  let choice = prompt("Enter your choice (Rock, Paper, Scissor)");
+  const choiceCapitalized = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+  if (choiceCapitalized === "Rock") {
+    return "Rock";
+  }
+  else if (choiceCapitalized === "Paper") {
+    return "Paper";
+  }
+  else if (choiceCapitalized === "Scissor") {
+    return "Scissor";
+  } else {
+    return "Invalid Choice";
+  }
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
